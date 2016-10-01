851cab3965e272b69ce5d6d61ac1dc1a953bc613
lin <458367123@qq.com>
2016-10-01 15:16:19 +0800

    user login

--- app/models.py
+++ app/models.py
@@ -1,21 +1,44 @@
-from . import db
+from . import db,login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 class Role(db.Model):
-    __tablename__ = 'roles'
-    id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String(64), unique=True)
-    users = db.relationship('User', backref='role', lazy='dynamic')
+	__tablename__ = 'roles'
+	id = db.Column(db.Integer, primary_key=True)
+	name = db.Column(db.String(64), unique=True)
+	users = db.relationship('User', backref='role', lazy='dynamic')
 
-    def __repr__(self):
-        return '<Role %r>' % self.name
+	def __repr__(self):
+		return '<Role %r>' % self.name
 
 
-class User(db.Model):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(64), unique=True, index=True)
-    role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+class User(UserMixin,db.Model):
+	__tablename__ = 'users'
+	id = db.Column(db.Integer, primary_key=True)
+	username = db.Column(db.String(64), unique=True, index=True)
+	role_id = db.Column(db.Integer, db.ForeignKey('roles.id'))
+	
+	email = db.Column(db.String(64), unique=True, index=True)
+	password_hash = db.Column(db.String(128))
+	
+	#check the password is right or not
+	@property
+	def password(self):
+		raise AttributeError('password is not a readable attribute')
+	
+	#use password to create the password_lists
+	@password.setter
+	def password(self, password):
+		self.password_hash = generate_password_hash(password)
+		
+	#check the password is pair the database_password or not
+	def verify_password(self, password):
+		return check_password_hash(self.password_hash, password)
 
-    def __repr__(self):
-        return '<User %r>' % self.username
+	def __repr__(self):
+		return '<User %r>' % self.username
+
+@login_manager.user_loader
+def load_user(user_id):
+	return User.query.get(int(user_id))
