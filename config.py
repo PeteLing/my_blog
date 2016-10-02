da152a17024c6a2287f90d021777e012c2b77e55
lin <458367123@qq.com>
2016-10-02 23:39:22 +0800

    orginal

--- config.py
+++ config.py
@@ -3,42 +3,43 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-	SECRET_KEY = os.environ.get('SECRET_KEY') or 'can you guess'
-	SQLALCHEMY_COMMIT_ON_TEARDOWN = True
-	MAIL_SERVER = 'smtp.qq.com'
-	MAIL_PORT = 587
-	MAIL_USE_TLS = True
-	MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-	MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
-	FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-	FLASKY_MAIL_SENDER = 'Flasky Admin <example@163.com>'
-	FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'hard to guess string'
+    SQLALCHEMY_COMMIT_ON_TEARDOWN = True
+    MAIL_SERVER = 'smtp.googlemail.com'
+    MAIL_PORT = 587
+    MAIL_USE_TLS = True
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <flasky@example.com>'
+    FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
-	@staticmethod
-	def init_app(app):
-		pass
+    @staticmethod
+    def init_app(app):
+        pass
 
 
 class DevelopmentConfig(Config):
-	DEBUG = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
-			'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
+    DEBUG = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DEV_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-dev.sqlite')
 
 
 class TestingConfig(Config):
-	TESTING = True
-	SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
-			'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = os.environ.get('TEST_DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data-test.sqlite')
 
 
 class ProductionConfig(Config):
-	SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
-			'sqlite:///' + os.path.join(basedir, 'data.sqlite')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'data.sqlite')
 
 
 config = {
-	'development': DevelopmentConfig,
-	'testing': TestingConfig,
-	'production': ProductionConfig,
-	'default': DevelopmentConfig
+    'development': DevelopmentConfig,
+    'testing': TestingConfig,
+    'production': ProductionConfig,
+
+    'default': DevelopmentConfig
 }
