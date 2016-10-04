1c09684ccdc1a6f8ba19984b87adc25fc3060047
lin <458367123@qq.com>
2016-10-04 14:59:59 +0800

    fix some bug

--- app/models.py
+++ app/models.py
@@ -1,11 +1,12 @@
 from . import db , login_manager
 from flask_login import UserMixin,AnonymousUserMixin
 from werkzeug.security import generate_password_hash, check_password_hash
-from flask import current_app
+from flask import current_app,request
 from itsdangerous import TimedJSONWebSignatureSerializer as Serializer
 from datetime import datetime
 import hashlib
-from flask import request
+from markdown import markdown
+import bleach
 
 #opertaion_action
 class Permission:
@@ -206,6 +207,7 @@ class Post(db.Model):
 	body = db.Column(db.Text)
 	timestamp = db.Column(db.DateTime,index=True,default=datetime.utcnow)
 	author_id = db.Column(db.Integer,db.ForeignKey('users.id'))
+	body_html = db.Column(db.Text)
 	
 	@staticmethod
 	def generate_fake(count=100):
@@ -220,7 +222,17 @@ class Post(db.Model):
 					author=u)
 			db.session.add(p)
 			db.session.commit()
+	
+	@staticmethod
+	def on_changed_body(target, value, oldvalue, initiator):
+		allowed_tags = ['a', 'abbr', 'acronym', 'b', 'blockquote', 'code',
+						'em', 'i', 'li', 'ol', 'pre', 'strong', 'ul',
+						'h1', 'h2', 'h3', 'p']
+		target.body_html = bleach.linkify(bleach.clean(
+				markdown(value, output_format='html'),
+				tags=allowed_tags, strip=True))
 
+db.event.listen(Post.body, 'set', Post.on_changed_body)
 login_manager.anonymous_user = AnonymousUser
 
 @login_manager.user_loader
